98de991b8aa75e3a94c3a879c74dc476ad08dcba
Martin Exler <m.exi@seznam.cz>
2021-06-01 22:16:17 +0200

    prvni commit

---
+++ pomocny_script_1.sql
@@ -0,0 +1,56 @@
+
+
+SELECT 
+	base.date,
+	base.country,
+	base.confirmed,
+	c.population,
+	ct.tests_performed 
+FROM covid19_basic_differences base
+LEFT JOIN countries c 
+	ON base.country = c.country
+LEFT JOIN covid19_tests ct 
+	ON base.`date` = ct.`date` 
+	AND base.country = ct.country 
+
+
+SELECT 
+	*
+FROM covid19_tests ct
+WHERE country = 'Czech Republic'
+ORDER BY `date` DESC
+
+
+
+-- urceni binarni promenne pro vikend
+SELECT
+	`date`,
+	CASE WHEN WEEKDAY(`date`) IN (0,1,2,3,4) THEN 0
+		 ELSE 1 END Weekend_or_not
+FROM covid19_basic_differences base;
+
+-- HDP na obyvatele
+SELECT 
+	country,
+	`year`,
+	GDP,
+	population,
+	ROUND(GDP/population, 2) AS na_obyvatele
+FROM economies e ;
+
+-- hustota zalidneni
+SELECT 
+	country,
+	surface_area,
+	population_density,
+	ROUND(SUM(surface_area * population_density) / SUM(surface_area), 2) AS hustota_zalidneni
+FROM countries c 
+GROUP BY country 
+
+
+
+
+
+
+
+
