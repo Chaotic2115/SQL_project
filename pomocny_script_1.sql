f30f83905ecd89cba26a82efb859b89cbe031c3b
Martin Exler <m.exi@seznam.cz>
2021-06-04 22:07:37 +0200

    diff life expectancy, GINI, median

--- pomocny_script_1.sql
+++ pomocny_script_1.sql
@@ -34,12 +34,42 @@ FROM economies e ;
 SELECT 
 	country,
 	surface_area,
-	population_density,
+	round(population_density, 2) AS a,
 	ROUND(SUM(surface_area * population_density) / SUM(surface_area), 2) AS hustota_zalidneni
 FROM countries c 
-GROUP BY country 
+GROUP BY country ;
 
+-- GINI koef, detska umrtnost
+SELECT 
+	gini,
+	mortaliy_under5 
+FROM economies e ;
 
+-- median veku v roce 2018
+SELECT 
+	country,
+	median_age_2018 
+FROM countries c 
+
+-- rozdil doby doziti 1965 a 2015
+SELECT 
+	a.country,
+	a.life_exp_1965,
+	b.life_exp_2015,
+	(b.life_exp_2015 - a.life_exp_1965) AS diff_in_life_expectancy
+FROM(
+	SELECT 
+		country,
+		life_expectancy AS life_exp_1965
+	FROM life_expectancy 
+	WHERE `year` = 1965) AS a
+JOIN (
+	SELECT 
+		country,
+		life_expectancy AS life_exp_2015
+	FROM life_expectancy 
+	WHERE `year` = 2015) AS b
+  ON a.country = b.country;
 
 
 
